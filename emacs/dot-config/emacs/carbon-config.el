9b1e455f269f3d093c1dc65355ab37e76faa7417
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2025-12-08 22:19:40 -0800

    add additional states for tasks

--- emacs/dot-config/emacs/carbon-config.el
+++ emacs/dot-config/emacs/carbon-config.el
@@ -5,3 +5,11 @@
 (setq org-default-notes-file (concat org-directory "/capture.org"))
 ;; custom variables for common files
 (setq my-org-tasks-file (concat org-directory "/tasks.org"))
+
+;;; custom todo keywords
+;;; https://orgmode.org/manual/Multiple-sets-in-one-file.html
+;;; https://orgmode.org/manual/Fast-access-to-TODO-states.html
+;; (setq org-todo-keywords
+;;       '((sequence "PENDING(p)" "UPNEXT(u)" "TODO(t)" "WORKING(w)" "|" "DONE(d)") ; basic state workflow
+;;         (sequence "FINDING(f)" | "BUG(b)" "SAFE(s)") ; bug workflow (find -> validation -> resolution)
+;;         ))
