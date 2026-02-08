1f8e6848f6951c4c63dbdd7ce4c035bcefdc3902
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2026-02-08 11:04:41 -0800

    highlight on yank autocmd

--- tty/dot-config/nvim/lua/hyper/autocmd.lua
+++ tty/dot-config/nvim/lua/hyper/autocmd.lua
@@ -18,9 +18,24 @@ vim.api.nvim_create_autocmd("FileType", {
   command = "setlocal colorcolumn=100 textwidth=100 conceallevel=2 wrap"
 })
 
+-- lua: tabstop=2
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "lua",
   command = "setlocal expandtab tabstop=2 shiftwidth=2 softtabstop=2"
+})
+
+
+-- highlight around yanked text to make it clear what was yanked
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  desc = "Highlight selection on yank",
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch", -- use the IncSearch highlight group
+      timeout = 500,         -- highlight for 500 milliseconds
+    })
+  end,
 })
 
 -- Return to last edited line upon reopen
