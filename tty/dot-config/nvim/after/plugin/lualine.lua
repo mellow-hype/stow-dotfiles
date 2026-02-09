335d43fe16dd1f05a46b72f25320219533c5fe23
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2026-02-09 00:09:19 -0800

    add custom selectioncount function for lualine

--- tty/dot-config/nvim/after/plugin/lualine.lua
+++ tty/dot-config/nvim/after/plugin/lualine.lua
@@ -1,23 +1,44 @@
+
 -- init for lualine
+local custom_selectioncount = function()
+  local mode = vim.fn.mode()
+  -- exit if not in a visual/selection mode
+  if mode ~= "v" and mode ~= "V" and mode ~= "\22" then
+    return ""
+  end
+  local wc = vim.fn.wordcount()
+  local chars = wc.visual_chars
+  if chars == nil then
+    return ""
+  end
+  return tostring(chars)
+end
+
 
 require('lualine').setup{
   options = {
-    theme = 'base16',
+    theme = 'auto',
   },
   sections = {
     -- path=4 means parent/filename format
     lualine_c = {
       { 'filename', path = 4 },
     },
+    -- right side ------------
     lualine_x = {
-      'filetype',
       'encoding',
       {
         'fileformat',
         symbols = { unix = 'unix', dos = 'dos', mac = 'mac' }
       },
+      'filetype',
     },
-    lualine_y = {},
+    lualine_y = {
+      { custom_selectioncount }
+    },
+    lualine_z = {
+      {'location'},
+    },
   },
   inactive_sections = {
     lualine_c = {{ 'filename', path = 4 }},
